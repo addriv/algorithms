8d23b68e0e67d0030786180fd08632bb430ba27e
addriv <adrivero89@gmail.com>
2017-10-24 15:20:02 -0700

    Add install order

--- graphs_topological_sort/lib/install_order.rb
+++ graphs_topological_sort/lib/install_order.rb
@@ -7,8 +7,30 @@
 
 # Import any files you need to
 
-
+require_relative 'graph'
+require_relative 'topological_sort'
 
 def install_order(arr)
+  check = Hash.new
+  min_id = nil
+  max_id = nil
+  vertices = []
+  
+  arr.each do |tuple|
+    tuple.each do |package_id|
+      if check[package_id].nil?
+        max_id = package_id if max_id.nil? || max_id < package_id
+        min_id = package_id if min_id.nil? || min_id > package_id
+        vertex = Vertex.new(package_id)
+        check[package_id] = vertex
+        vertices.push(vertex)
+      end
+    end
 
+    Edge.new(check[tuple[1]], check[tuple[0]])
+  end
+
+  result = kahn_topological_sort(vertices).map { |vertex| vertex.value }
+  (min_id..max_id).each { |id| result.push(id) unless check[id] }
+  result
 end
