10849b3d93103526696203fb60e18f5e98d41fac
addriv <adrivero89@gmail.com>
2017-10-25 21:50:16 -0700

    Solved making anagrams

--- hacker_rank/data_structures/strings_making_anagrams/my_solution.rb
+++ hacker_rank/data_structures/strings_making_anagrams/my_solution.rb
@@ -1,6 +1,21 @@
 # Find the number of characters in common
 # Subtract from the total length of strings
+# All characters in both strings are lowercase
 
 def making_anagrams(string1, string2)
+  matching_chars = 0
+  visited_chars = Hash.new
+    
+  longest_string = string1.length > string2.length ? string1 : string2
+    
+  longest_string.each_char do |ch|
+    if visited_chars[ch]
+      next
+    else
+      visited_chars[ch] = true
+      matching_chars += [string1.count(ch), string2.count(ch)].min
+    end
+  end
   
+  string1.length + string2.length - matching_chars * 2
 end
