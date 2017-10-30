66db36762cd96471cc760cffe140dbbdf8e83e82
addriv <adrivero89@gmail.com>
2017-10-30 13:56:48 -0700

    Solve queues with two stacks

---
+++ hackerrank/cracking_the_coding_interview/data_structures/queues_two_stacks/a_tale_of_two_stacks.rb
@@ -0,0 +1,23 @@
+# Get number of queries
+q = gets.chomp.to_i
+
+# Helper method for parsing queries
+def queue_stacks(str, stack)
+  input = str.split.map(&:to_i)
+  if input[0] == 1
+    stack.push(input[1])
+  elsif input[0] == 2
+    stack.shift
+  elsif input[0] == 3
+    puts stack[0]
+  end
+  
+  stack
+end
+
+stack = []
+
+q.times do
+  input = gets.chomp
+  stack = queue_stacks(input, stack)
+end
