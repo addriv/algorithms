c426d9722618812820a0165646d040978a2ee59b
addriv <adrivero89@gmail.com>
2017-11-01 17:56:58 -0700

    Fix ruby solution for is binary tree

--- hackerrank/cracking_the_coding_interview/data_structures/trees_binary_search/is_binary_search_tree.rb
+++ hackerrank/cracking_the_coding_interview/data_structures/trees_binary_search/is_binary_search_tree.rb
@@ -9,14 +9,20 @@ class Node
   end 
 end
 
-def BFS(node, data_hash)
-  return false if node.nil?
-  
+def BFS(node, min, max)
+  # Return true if tree is empty
+  return true if node.nil?
+
+  # Check against min and max constraints
+  return false if node.data < min || node.data > max
+
+  #Else recursively check subtrees and update min and max
+  return BFS(node.left, min, node.data - 1) && BFS(node.right, node.data + 1, max)
 end
 
-def checkBST(root):
+def checkBST(root)
   return BFS(root, -1, 1 * 10^4)
-   
+end
 
 # Test case tree
 a = Node.new(4)
@@ -25,7 +31,7 @@ c = Node.new(1)
 d = Node.new(3)
 e = Node.new(6)
 f = Node.new(5)
-g = Node.new(5)
+g = Node.new(7)
 
 a.left = b
 a.right = e
