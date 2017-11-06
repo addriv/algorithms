e35aad4bf9264e7fa94db832308b6ac6040fc281
addriv <adrivero89@gmail.com>
2017-11-05 18:43:35 -0800

    Create heaps class

--- hackerrank/cracking_the_coding_interview/data_structures/heaps_running_median/find_the_running_median.rb
+++ hackerrank/cracking_the_coding_interview/data_structures/heaps_running_median/find_the_running_median.rb
@@ -3,3 +3,21 @@ a = Array.new(n)
 for a_i in (0..n-1)
     a[a_i] = gets.strip.to_i
 end
+
+# Create heap class
+class Heap
+  def initialize
+    @heap = []
+  end
+
+  def parent_idx(child_idx, length)
+    return nil if child_idx == 0
+
+    (child_idx - 1) / 2
+  end
+
+  def child_idx(parent_idx, length)
+    children = [parent_idx * 2 +1, parent_idx * 2 + 2]
+    return nil if children.max > length - 1
+  end   
+end
