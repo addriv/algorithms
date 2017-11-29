9de6848a497ea1ec020c05997933ae9f4c54ee36
addriv <adrivero89@gmail.com>
2017-11-29 14:36:45 -0800

    Solve counting inversions

--- hackerrank/cracking_the_coding_interview/algorithms/merge_sort_counting_inversions/counting_inversions.js
+++ hackerrank/cracking_the_coding_interview/algorithms/merge_sort_counting_inversions/counting_inversions.js
@@ -1,22 +1,75 @@
-function mergeSort(nums1, nums2) {
-  const sorted = [];
-  
-  const arr1 = Array.from(nums1);
-  const arr2 = Array.from(nums2);
-  while (arr1.length > 0 && arr2.length > 0) {
-    if (arr1[0] >= arr2[0]) {
-      sorted.push(arr2.shift());
-    }
-    else {
-      sorted.push(arr1.shift());
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+  input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+  input_stdin_array = input_stdin.split("\n");
+  main();
+});
+
+function readLine() {
+  return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function countInversions(arr, len = arr.length) {
+  // base case return nums if length <= 1
+  if (arr.length <= 1) {
+    return [arr, 0];
+  }
+
+  let numInversions = 0;
+
+  const mid = Math.floor(arr.length / 2);
+  const arr1 = arr.slice(0, mid);
+  const arr2 = arr.slice(mid);
+
+  const left = countInversions(arr1, len);
+  const right = countInversions(arr2, len);
+
+  const merged = merge(left[0], right[0], left[1] + right[1]);
+  numInversions += merged[1];
+
+  if (merged[0].length === len) {
+    return numInversions;
+  }
+  else {
+    return [merged[0], numInversions];
+  }
+}
+
+function merge(left, right, inversions) {
+  let sorted = [];
+  while (left.length > 0 && right.length > 0) {
+    if (left[0] > right[0]) {
+      sorted.push(right.shift());
+      inversions += left.length;
+    } else {
+      sorted.push(left.shift());
     }
   }
 
-  return sorted.concat(nums1)
-               .concat(nums2);
+  sorted = sorted.concat(left).concat(right);
+
+  return [sorted, inversions];
 }
 
-const nums1 = [1,2,3];
-const nums2 = [4,5,6];
+function main() {
+  var t = parseInt(readLine());
+  for (var a0 = 0; a0 < t; a0++) {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    var result = countInversions(arr);
+    process.stdout.write("" + result + "\n");
+  }
 
-mergeSort(nums1, nums2);
+}
