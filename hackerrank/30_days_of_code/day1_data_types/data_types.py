c5418ef88ba2dfd32c0460560a3f9cab25e73c82
addriv <adrivero89@gmail.com>
2017-12-06 11:19:58 -0800

    Add python solution for day 1: data types

---
+++ hackerrank/30_days_of_code/day1_data_types/data_types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+j = int(input());
+k = float(input());
+l = input();
+
+# Print the sum of both integer variables on a new line.
+print(i + j);
+
+# Print the sum of the double variables on a new line.
+print(k + d);
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print("{}{}".format(s, l));
