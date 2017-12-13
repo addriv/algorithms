f106c5767d727cab833e1d28066702b80568e9a3
addriv <adrivero89@gmail.com>
2017-12-12 19:18:57 -0800

    Start append delete

---
+++ hackerrank/tracks/algorithms/append_delete/append_delete.rb
@@ -0,0 +1,7 @@
+# Given string (s), can perform appending a letter OR deleting last letter
+# Given k, and two string, s and t, determine if s can be converted to t by performing
+# exactly k operations. Print YES or NO
+
+def append_delete(k,s,t)
+  
+end
