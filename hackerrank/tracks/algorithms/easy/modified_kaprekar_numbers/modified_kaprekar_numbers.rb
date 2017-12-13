701397d4cf421a5af27c2d747e663acf2d1dfdba
addriv <adrivero89@gmail.com>
2017-12-13 14:00:46 -0800

    First attempt

--- hackerrank/tracks/algorithms/easy/modified_kaprekar_numbers/modified_kaprekar_numbers.rb
+++ hackerrank/tracks/algorithms/easy/modified_kaprekar_numbers/modified_kaprekar_numbers.rb
@@ -1,3 +1,24 @@
-def modified_kaprekar_numbers(p, q)
+def modified_kaprekars(p,q)
+  modified_kaprekars = []
+
+  (p..q).each do |n|
+    modified_kaprekars << n if is_modified_kaprekar?(n)
+  end
   
+  modified_kaprekars.join(" ")
 end
+
+def is_modified_kaprekar?(n)
+  square = (n ** 2).to_s
+  # square.to_s
+
+  i = 0
+  while square[0..i].to_i <= n
+    break if square[i + 1..-1].to_i == 0
+    return true if square[0..i].to_i + square[i + 1..-1].to_i == n
+    i += 1
+  end
+
+  false
+end
+
