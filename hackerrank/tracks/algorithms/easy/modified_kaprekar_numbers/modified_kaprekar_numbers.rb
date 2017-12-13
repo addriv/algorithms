ff3b5e79e362fcf4f40d545c262accad76c8e1c0
addriv <adrivero89@gmail.com>
2017-12-13 14:13:01 -0800

    Next test

--- hackerrank/tracks/algorithms/easy/modified_kaprekar_numbers/modified_kaprekar_numbers.rb
+++ hackerrank/tracks/algorithms/easy/modified_kaprekar_numbers/modified_kaprekar_numbers.rb
@@ -5,16 +5,22 @@ def modified_kaprekars(p,q)
     modified_kaprekars << n if is_modified_kaprekar?(n)
   end
   
-  modified_kaprekars.join(" ")
+  if modified_kaprekars.empty?
+    "INVALID RANGE"
+  else
+    modified_kaprekars.join(" ")
+  end
 end
 
 def is_modified_kaprekar?(n)
+  return true if n == 1
+  
   square = (n ** 2).to_s
-  # square.to_s
 
   i = 0
   while square[0..i].to_i <= n
     break if square[i + 1..-1].to_i == 0
+
     return true if square[0..i].to_i + square[i + 1..-1].to_i == n
     i += 1
   end
