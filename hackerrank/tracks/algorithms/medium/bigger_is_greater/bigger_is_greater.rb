3fd336451bfd39632e14bd98a955f96802c70965
addriv <adrivero89@gmail.com>
2017-12-13 13:37:36 -0800

    Include problem statement

--- hackerrank/tracks/algorithms/medium/bigger_is_greater/bigger_is_greater.rb
+++ hackerrank/tracks/algorithms/medium/bigger_is_greater/bigger_is_greater.rb
@@ -1,4 +1,6 @@
-# Find which substring is ordered from smallest
+# Given a word, rearrange the letters to construct another word s in such a way that is
+# lexicographically greater than w. In case of multiple answers find the lexicographically
+# smallest one among them 
 
 # Steps
 # 1. Find the longest non-increasing suffix from the right
@@ -7,18 +9,15 @@
 # 4. Swap successor with pivot
 # 5. Reverse suffix
 
-# Replace the next letter with smallest letter
-# sort the remaining
+
 def bigger_is_greater(str)
-  return "no answer" if str.length == 2 && str.reverse == str
-  
   transformed = str.dup
   i = str.length - 1
   while i > 0 && transformed[i - 1] >= transformed[i]
     i -= 1
   end
 
-  # pivot = i - 1
+  return "no answer" if i == 0
   suffix = transformed[i..-1].reverse
   preffix = transformed[0...i]
 
