375b05bc87cc8893cd8a13aadc7a9417a67686a5
addriv <adrivero89@gmail.com>
2017-12-13 13:26:42 -0800

    Submit attempt

--- hackerrank/tracks/algorithms/medium/bigger_is_greater/bigger_is_greater.rb
+++ hackerrank/tracks/algorithms/medium/bigger_is_greater/bigger_is_greater.rb
@@ -10,5 +10,24 @@
 # Replace the next letter with smallest letter
 # sort the remaining
 def bigger_is_greater(str)
- 
+  return "no answer" if str.length == 2 && str.reverse == str
+  
+  transformed = str.dup
+  i = str.length - 1
+  while i > 0 && transformed[i - 1] >= transformed[i]
+    i -= 1
+  end
+
+  # pivot = i - 1
+  suffix = transformed[i..-1].reverse
+  preffix = transformed[0...i]
+
+  suffix.each_char.with_index do |ch,idx|
+    if ch > preffix[-1]
+      preffix[-1], suffix[idx] = suffix[idx], preffix[-1]
+      break
+    end 
+  end
+
+  preffix + suffix
 end
