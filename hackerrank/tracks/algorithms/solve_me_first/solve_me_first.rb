1bfcef2583e5cfb622ac74d5a95a6378a761daf1
addriv <adrivero89@gmail.com>
2017-12-12 19:08:25 -0800

    Finish solve me first

--- hackerrank/tracks/algorithms/solve_me_first/solve_me_first.rb
+++ hackerrank/tracks/algorithms/solve_me_first/solve_me_first.rb
@@ -1,1 +1,10 @@
 # Return the sum of two integers read from stdin.
+
+def solve_me_first (a, b)
+  return a + b
+end
+
+val1 = gets.to_i
+val2 = gets.to_i
+sum = solve_me_first(val1,val2)
+print (sum)
