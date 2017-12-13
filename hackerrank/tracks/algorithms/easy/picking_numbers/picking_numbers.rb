c56bb758b2c0777b8b5fcaa8257cfbed17532a27
addriv <adrivero89@gmail.com>
2017-12-12 20:12:23 -0800

    Attempt first solution

--- hackerrank/tracks/algorithms/easy/picking_numbers/picking_numbers.rb
+++ hackerrank/tracks/algorithms/easy/picking_numbers/picking_numbers.rb
@@ -1,1 +1,18 @@
 # Given an array of integers, find the max number pairs that are <= 1 from each other
+# Naive solution, O(n) = n^2 to check each pair iteratively
+# Better solution, iterate through once. Use hash table to check if values exist
+
+def picking_numbers(nums)
+  integers = Hash.new
+  count = 0
+
+  nums.each do |n|
+    count += 1 if integers[n - 1]
+    count += 1 if integers[n + 1]
+    count += 1 if integers[n]
+
+    integers[n] += 1
+  end
+
+  count
+end
