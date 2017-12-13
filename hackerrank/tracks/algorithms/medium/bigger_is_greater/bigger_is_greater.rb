ee0340dff057907c81651de01a66d49d8cf30e6b
addriv <adrivero89@gmail.com>
2017-12-13 12:39:00 -0800

    Attempt solution 2

--- hackerrank/tracks/algorithms/medium/bigger_is_greater/bigger_is_greater.rb
+++ hackerrank/tracks/algorithms/medium/bigger_is_greater/bigger_is_greater.rb
@@ -1,9 +1,30 @@
-
+# Find which substring is ordered from smallest
+# Replace the next letter with smallest letter
+# sort the remaining
 def bigger_is_greater(str)
+  transformed = str.dup
+
   i = -2
-  while i > -str.length
-    break if str[i..-1] < str[i..-1].reverse
+  while i > -transformed.length
+    break if transformed[i..-1] < transformed[i..-1].reverse
+    i -= 1
   end
-  str[i], str[i + 1] = str[i + 1], str[i]
-  str
+  left = transformed.split("")
+            .take(i)
+  right = transformed.split("")
+             .drop(i)
+             .sort
+
+  right.each_with_index do |ch,idx|
+    next if ch == str[i]
+    if ch > str[i]
+      right[0], right[idx] = right[idx], right[0] 
+      break
+    end
+  end
+
+  left + right
+
+  # str[i], str[i + 1] = str[i + 1], str[i]
+  # str
 end
