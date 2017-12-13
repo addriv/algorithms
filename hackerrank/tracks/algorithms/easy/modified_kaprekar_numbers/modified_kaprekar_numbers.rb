8fdda74bccd0f6b0a5bd2892f84e593cd8708ef8
addriv <adrivero89@gmail.com>
2017-12-13 14:20:14 -0800

    Start new approach, square must be split evenly

--- hackerrank/tracks/algorithms/easy/modified_kaprekar_numbers/modified_kaprekar_numbers.rb
+++ hackerrank/tracks/algorithms/easy/modified_kaprekar_numbers/modified_kaprekar_numbers.rb
@@ -13,18 +13,7 @@ def modified_kaprekars(p,q)
 end
 
 def is_modified_kaprekar?(n)
-  return true if n == 1
-  
-  square = (n ** 2).to_s
 
-  i = 0
-  while square[0..i].to_i <= n
-    break if square[i + 1..-1].to_i == 0
 
-    return true if square[0..i].to_i + square[i + 1..-1].to_i == n
-    i += 1
-  end
-
-  false
 end
 
