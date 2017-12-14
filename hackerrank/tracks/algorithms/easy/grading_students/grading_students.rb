a3e48d3d96c7c57644934eab7377bcf788c5f92d
addriv <adrivero89@gmail.com>
2017-12-14 14:25:36 -0800

    Solve grading students

--- hackerrank/tracks/algorithms/easy/grading_students/grading_students.rb
+++ hackerrank/tracks/algorithms/easy/grading_students/grading_students.rb
@@ -1,5 +1,14 @@
 # Given a grade, round to the next multiple of 5 if it is less than 3 away
 # Do not round grades less than 38 as it will be a failing grade
 
-def grading_students(grade)
+def round(grade)
+  if grade < 38 || grade % 5 < 3
+    grade
+  else
+    (grade / 5) * 5 + 5
+  end
+end
+
+def solve(grades)
+  grades.map { |grade| round(grade) } 
 end
