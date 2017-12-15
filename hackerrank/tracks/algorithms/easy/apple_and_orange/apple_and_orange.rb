58e08d28e55a2ac2f4a122c641019ffce4e0769b
addriv <adrivero89@gmail.com>
2017-12-15 12:12:40 -0800

    Include problem statement

--- hackerrank/tracks/algorithms/easy/apple_and_orange/apple_and_orange.rb
+++ hackerrank/tracks/algorithms/easy/apple_and_orange/apple_and_orange.rb
@@ -1,3 +1,12 @@
+# Determine how many apples and how many oranges will fall on Sam's house given
+# the following variables
+
+# s,t = the left and right bounds of Sam's house
+# a = the location of the apple tree
+# b = the location of the orange tree
+# distance the apples and oranges fall, positive numbers mean fruit fell to the
+# right of the tree
+
 # Print numbers of apples as first line
 # Print number of oranges as second line
 def apples_and_oranges(s, t, a, b, apples, oranges)
