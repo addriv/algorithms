944a09dd687dec9f116544ffabf603e130757f38
addriv <adrivero89@gmail.com>
2017-12-16 23:35:53 -0800

    Add problem statement

---
+++ hackerrank/tracks/algorithms/easy/kangaroo/kangaroo.rb
@@ -0,0 +1,3 @@
+# Given a line of 4 integers for position(x) and velocity(v) for two kangaroos
+# determine if the two kangaroos ever land at the same location at the same time
+# Ex: 0(x1) 3(v1) 4(x2) 2(v2)
