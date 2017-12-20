20c487a2a852752dd84ca14e70ed26d53cba31cb
addriv <adrivero89@gmail.com>
2017-12-20 14:36:12 -0800

    Update problem statement

--- hackerrank/tracks/algorithms/medium/non_divisible_subset/non_divisible_subset.rb
+++ hackerrank/tracks/algorithms/medium/non_divisible_subset/non_divisible_subset.rb
@@ -1,7 +1,8 @@
-# Given a set, S, of n distinct integers, print the size of maximal subsets where
-# the sum of any 2 numbers is not evenly divisible by k
+# Given a set, S, of n distinct integers, print the size of the largest possible 
+# subset of integers where no pair sums to k
 
 # Ex. k = 3, S = [1,7,2,4] => Result = 3
+
 
 # Naive solution, two iterators i & j, test each pair. O(n) = n^2
 n, k = gets.strip.split.map(&:to_i)
@@ -25,3 +26,13 @@ def non_divisible_subset(s, k)
 end
 
 puts non_divisible_subset(s, k)
+
+s = [2, 7, 12, 17, 22]
+
+# More efficient solution, hash tables
+# Iterate through all of S, create a hash table with n % k as the key
+# if matching pair exists in table, increase count
+
+def non_divisible_subset()
+
+end
