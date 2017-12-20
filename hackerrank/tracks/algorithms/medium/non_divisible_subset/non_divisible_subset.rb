5f6c36ecb0cc036215e4a995f3d28c799964dabf
addriv <adrivero89@gmail.com>
2017-12-20 14:15:18 -0800

    Update non divisible subset problem statement

--- hackerrank/tracks/algorithms/medium/non_divisible_subset/non_divisible_subset.rb
+++ hackerrank/tracks/algorithms/medium/non_divisible_subset/non_divisible_subset.rb
@@ -1,1 +1,4 @@
-Given a set, , of  distinct integers, print the size of a maximal subset, , of  where the sum of any  numbers in  is not evenly divisible by .
+# Given a set, S, of n distinct integers, print the size of maximal subsets where
+# the sum of any 2 numbers is not evenly divisible by k
+
+# Ex. k = 3, S = [1,7,2,4] => Result = 3
