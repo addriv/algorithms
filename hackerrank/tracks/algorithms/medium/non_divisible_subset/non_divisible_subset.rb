416f16b2b6fd5633b0b175273df7853d360ba8ea
addriv <adrivero89@gmail.com>
2017-12-20 14:29:10 -0800

    Submit naive solution

--- hackerrank/tracks/algorithms/medium/non_divisible_subset/non_divisible_subset.rb
+++ hackerrank/tracks/algorithms/medium/non_divisible_subset/non_divisible_subset.rb
@@ -2,3 +2,26 @@
 # the sum of any 2 numbers is not evenly divisible by k
 
 # Ex. k = 3, S = [1,7,2,4] => Result = 3
+
+# Naive solution, two iterators i & j, test each pair. O(n) = n^2
+n, k = gets.strip.split.map(&:to_i)
+s = gets.strip.split.map(&:to_i)
+
+def non_divisible_subset(s, k)
+  count = 0
+
+  i = 0
+  j = 1
+  while i < j
+    j = i + 1
+    while j < s.length
+      count += 1 if (s[i] + s[j]) % k != 0
+      j += 1    
+    end
+    i += 1
+  end
+
+  count
+end
+
+puts non_divisible_subset(s, k)
