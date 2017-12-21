f1ad112943ddd26e67d10ad9d4969cd577f99025
addriv <adrivero89@gmail.com>
2017-12-20 16:09:06 -0800

    Clean up solution and annotate

--- hackerrank/tracks/algorithms/medium/non_divisible_subset/non_divisible_subset.rb
+++ hackerrank/tracks/algorithms/medium/non_divisible_subset/non_divisible_subset.rb
@@ -1,7 +1,6 @@
 # Given a set, S, of n distinct integers, print the size of the largest possible 
 # subset of integers where no pair sums to k
 # Ex. k = 3, S = [1,7,2,4] => Result = 3
-# [1, 1, 2, 1]
 
 # Naive solution, two iterators i & j. Get all subsets, push n into subset if matching
 # pair (k - modulo) does not exist in the subset
@@ -39,48 +38,41 @@ def naive_non_divisible_subsets(s, k)
   counts.max
 end
 
-# More efficient solution, hash tables
+# More efficient solution: Use hash tables for checking if matching pair integer exists
+# Start with max size which equals the length of S
+
 # Iterate through all of S, create a hash table with n % k as the key
 # if matching pair exists in table, increase count
 # Max size = s.length
-# Ex. k = 3, S = [1,7,2,4] => Result = 3
 def non_divisible_subset(s, k)
   # Initially set max size = s.length
   max_size = s.length
+
+  # Get modulos of k from S. Create a hash table for checking values in constant time
+  # Keys = modulos, values = count of modulo
   modulos = s.map { |n| n % k }
   modulos_hash = Hash.new(0)  
   modulos.each { |modulo| modulos_hash[modulo] += 1 }
 
-  # TEST
-  subsets = []
-
+  # Iterate through the modulos
+  # Create a visted hash to skip modulos that have already been checked
   visited = Hash.new
   modulos_hash.each do |modulo, count|
     pair = k - modulo
+    # If modulo is zero or the matching pair is equal to the modulo
+    # only one can exist in subset, subtract all but 1 from max_size
+    # Else, subtract the lesser count between modulo and its pair
     if modulo == 0 || pair == modulo
       max_size -= (modulos_hash[modulo] - 1)
-
-      # TEST
-      subsets << modulo
     elsif modulos_hash[pair]
       next if visited[pair]
       min_pair = [modulos_hash[modulo], modulos_hash[pair]].min
       max_size -= min_pair
       visited[modulo] = true
-      
-      # TEST
-      max_pair = [modulos_hash[modulo], modulos_hash[pair]].max
-      valid = modulos_hash[modulo] == max_pair ? modulo : pair
-      max_pair.times do
-        subsets << valid
-      end
     end
   end
 
   max_size
-
-  # TEST
-  # subsets
 end
 
 n, k = gets.strip.split.map(&:to_i)
