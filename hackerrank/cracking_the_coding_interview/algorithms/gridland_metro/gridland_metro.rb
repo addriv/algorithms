7a3b8cdb1a7d7f16a43c7ae031ab8356aac2b118
Adrian Rivero <adrivero89@gmail.com>
2018-04-13 11:52:18 -0700

    fix solution with tracks engulfed in another track

--- hackerrank/cracking_the_coding_interview/algorithms/gridland_metro/gridland_metro.rb
+++ hackerrank/cracking_the_coding_interview/algorithms/gridland_metro/gridland_metro.rb
@@ -1,17 +1,28 @@
 def gridlandMetro(n, m, k, tracks)
     n_cells = n * m
-    track_cells = Hash.new
-
+    track_cells = {}
     tracks.each do |track|
-        track_cells[track[0]] ||= {}
-        for track_i in (track[1]..track[2])
-            track_cells[track[0]][track_i] = 1
-        end
+        track_cells[track[0]] ||= []
+        track_cells[track[0]].push([track[1], track[2]])
     end
 
     n_track_cells = 0
 
-    track_cells.each { |row, cells|  n_track_cells += cells.keys.count }
+    # puts track_cells
 
-    n_cells - n_track_cells
+    track_cells.each do |row, tracks|
+        sorted = tracks.sort_by { |track| track[0] }
+        # puts tracks
+        sorted.each_with_index do |track, i|
+            if i == 0 || sorted[i - 1][1] < track[0]
+                n_track_cells += (track[1] - track[0] + 1)
+            elsif sorted[i - 1][0] <= track[0] && sorted[i - 1][1] >= track[1]
+                next
+            else
+                n_track_cells += (track[1] - track[0] + 1 - (sorted[i - 1][1] - track[0] + 1))
+            end
+        end
+    end
+
+   n_cells - n_track_cells
 end
