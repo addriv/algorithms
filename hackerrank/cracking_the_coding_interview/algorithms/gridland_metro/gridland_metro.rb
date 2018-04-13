edf6f4144e1bcdbb52bb881cbdba27062b0d0596
Adrian Rivero <adrivero89@gmail.com>
2018-04-13 11:07:18 -0700

    fix inefficient solution

--- hackerrank/cracking_the_coding_interview/algorithms/gridland_metro/gridland_metro.rb
+++ hackerrank/cracking_the_coding_interview/algorithms/gridland_metro/gridland_metro.rb
@@ -1,16 +1,17 @@
 def gridlandMetro(n, m, k, tracks)
     n_cells = n * m
-    track_cells = Hash.new({})
+    track_cells = Hash.new
+
     tracks.each do |track|
-        track_cells[track[0]] = {}
+        track_cells[track[0]] ||= {}
         for track_i in (track[1]..track[2])
             track_cells[track[0]][track_i] = 1
         end
     end
 
     n_track_cells = 0
-    track_cells.each { |row, cells|  n_track_cells += cells.keys.count }
 
+    track_cells.each { |row, cells|  n_track_cells += cells.keys.count }
 
     n_cells - n_track_cells
 end
